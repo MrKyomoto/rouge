bcb20523c5b4c98d4ccf60e63a63cb0a10cab060
kyomoto-pixelbook <2028566723@qq.com>
2026-05-19 23:28:31 +0800

    src: refactor components

---
+++ src/components.rs
@@ -0,0 +1,31 @@
+use bracket_lib::prelude::*;
+use specs::prelude::*;
+use specs_derive::Component;
+
+#[derive(Component)]
+pub struct Position {
+    pub x: i32,
+    pub y: i32,
+}
+
+#[derive(Component)]
+pub struct Renderable {
+    pub glyph: FontCharType,
+    pub fg: RGB,
+    pub bg: RGB,
+}
+
+#[derive(Component, Debug)]
+pub struct Player {}
+
+#[derive(Component, Default)]
+pub struct PlayerInput {
+    pub dx: i32,
+    pub dy: i32,
+}
+
+#[derive(Component)]
+pub struct Viewshed {
+    pub visible_tiles: Vec<Point>,
+    pub range: i32,
+}
