41be31b6335ed1fe4fc502b1de977479be0fd0d8
kyomoto-pixelbook <2028566723@qq.com>
2026-05-19 23:28:35 +0800

    src: update main

--- src/main.rs
+++ src/main.rs
@@ -1,133 +1,16 @@
 use bracket_lib::prelude::*;
 use specs::prelude::*;
-use specs_derive::Component;
-use std::cmp::{max, min};
 
+mod components;
 mod map;
 mod rect;
+mod systems;
 mod utils;
 
-use crate::map::TileType;
+use crate::components::*;
+use crate::map::{Map, TileType};
+use crate::systems::*;
 use crate::utils::*;
-
-#[derive(Component)]
-struct Position {
-    x: i32,
-    y: i32,
-}
-
-#[derive(Component)]
-struct Renderable {
-    glyph: FontCharType,
-    fg: RGB,
-    bg: RGB,
-}
-
-#[derive(Component)]
-struct LeftMover {}
-
-#[derive(Component, Debug)]
-struct Player {}
-
-#[derive(Component, Default)]
-struct PlayerInput {
-    dx: i32,
-    dy: i32,
-}
-
-// #[derive(Component, Debug)]
-struct InputSystem<'a> {
-    ctx: &'a mut BTerm,
-}
-
-struct PlayerMovementSystem {}
-impl<'a> System<'a> for PlayerMovementSystem {
-    type SystemData = (
-        ReadStorage<'a, Player>,
-        ReadStorage<'a, PlayerInput>,
-        ReadExpect<'a, Vec<TileType>>,
-        WriteStorage<'a, Position>,
-    );
-
-    fn run(&mut self, (_, inputs, map, mut positions): Self::SystemData) {
-        for (input, pos) in (&inputs, &mut positions).join() {
-            let dest_x = min(79, max(0, pos.x + input.dx));
-            let dest_y = min(49, max(0, pos.y + input.dy));
-            if map[flatten_index(dest_x, dest_y)] == TileType::Floor {
-                pos.x = dest_x;
-                pos.y = dest_y;
-            }
-        }
-    }
-}
-
-struct MapRenderSystem<'a> {
-    ctx: &'a mut BTerm,
-}
-
-impl<'a> System<'a> for MapRenderSystem<'a> {
-    type SystemData = ReadExpect<'a, Vec<TileType>>;
-
-    fn run(&mut self, map: Self::SystemData) {
-        let mut x = 0;
-        let mut y = 0;
-        for tile in map.iter() {
-            match tile {
-                TileType::Floor => {
-                    self.ctx.set(
-                        x,
-                        y,
-                        RGB::from_f32(0.5, 0.5, 0.5),
-                        RGB::from_f32(0., 0., 0.),
-                        to_cp437('.'),
-                    );
-                }
-                TileType::Wall => {
-                    self.ctx.set(
-                        x,
-                        y,
-                        RGB::from_f32(0., 1., 0.),
-                        RGB::from_f32(0., 0., 0.),
-                        to_cp437('#'),
-                    );
-                }
-            }
-            x += 1;
-            if x > COL - 1 {
-                x = 0;
-                y += 1;
-            }
-        }
-    }
-}
-
-struct EntityRenderSystem<'a> {
-    ctx: &'a mut BTerm,
-}
-impl<'a> System<'a> for EntityRenderSystem<'a> {
-    type SystemData = (ReadStorage<'a, Position>, ReadStorage<'a, Renderable>);
-
-    fn run(&mut self, (pos, render): Self::SystemData) {
-        for (pos, render) in (&pos, &render).join() {
-            self.ctx
-                .set(pos.x, pos.y, render.fg, render.bg, render.glyph);
-        }
-    }
-}
-
-struct LeftWalker {}
-impl<'a> System<'a> for LeftWalker {
-    type SystemData = (ReadStorage<'a, LeftMover>, WriteStorage<'a, Position>);
-
-    fn run(&mut self, (lefty, mut pos): Self::SystemData) {
-        for (_, pos) in (&lefty, &mut pos).join() {
-            pos.x -= 1;
-            if pos.x < 0 {
-                pos.x = 79;
-            }
-        }
-    }
-}
 
 struct State {
     ecs: World,
@@ -146,9 +29,10 @@ impl State {
         MapRenderSystem { ctx }.run_now(&self.ecs);
         EntityRenderSystem { ctx }.run_now(&self.ecs);
 
+        VisibilitySystem {}.run_now(&self.ecs);
+
         InputSystem { ctx }.run_now(&self.ecs);
 
-        LeftWalker {}.run_now(&self.ecs);
         PlayerMovementSystem {}.run_now(&self.ecs);
 
         self.ecs.maintain();
@@ -162,22 +46,22 @@ fn main() -> BError {
 
     let mut gs = State { ecs: World::new() };
 
-    let (map, rooms) = map::new_map_rooms_and_corridors();
+    let map = Map::new_map_rooms_and_corridors();
+    let (init_x, init_y) = map.rooms[0].center();
     gs.ecs.insert(map);
-    let (player_init_x, player_init_y) = rooms[0].center();
 
     gs.ecs.register::<Position>();
     gs.ecs.register::<Renderable>();
-    gs.ecs.register::<LeftMover>();
     gs.ecs.register::<Player>();
     gs.ecs.register::<PlayerInput>();
+    gs.ecs.register::<Viewshed>();
 
     // 玩家
     gs.ecs
         .create_entity()
         .with(Position {
-            x: player_init_x,
-            y: player_init_y,
+            x: init_x,
+            y: init_y,
         })
         .with(Renderable {
             glyph: to_cp437('@'),
@@ -186,6 +70,10 @@ fn main() -> BError {
         })
         .with(Player {})
         .with(PlayerInput::default())
+        .with(Viewshed {
+            visible_tiles: Vec::new(),
+            range: 8,
+        })
         .build();
 
     // // 怪物
@@ -203,42 +91,4 @@ fn main() -> BError {
     // }
     //
     main_loop(context, gs)
-}
-
-impl<'a> System<'a> for InputSystem<'a> {
-    type SystemData = (ReadStorage<'a, Player>, WriteStorage<'a, PlayerInput>);
-
-    fn run(&mut self, (players, mut inputs): Self::SystemData) {
-        for (_, input) in (&players, &mut inputs).join() {
-            input.dx = 0;
-            input.dy = 0;
-        }
-
-        match self.ctx.key {
-            None => {}
-            Some(key) => match key {
-                VirtualKeyCode::Left | VirtualKeyCode::Numpad4 | VirtualKeyCode::H => {
-                    for (_, input) in (&players, &mut inputs).join() {
-                        input.dx = -1;
-                    }
-                }
-                VirtualKeyCode::Right | VirtualKeyCode::Numpad6 | VirtualKeyCode::L => {
-                    for (_, input) in (&players, &mut inputs).join() {
-                        input.dx = 1;
-                    }
-                }
-                VirtualKeyCode::Up | VirtualKeyCode::Numpad8 | VirtualKeyCode::K => {
-                    for (_, input) in (&players, &mut inputs).join() {
-                        input.dy = -1;
-                    }
-                }
-                VirtualKeyCode::Down | VirtualKeyCode::Numpad2 | VirtualKeyCode::J => {
-                    for (_, input) in (&players, &mut inputs).join() {
-                        input.dy = 1;
-                    }
-                }
-                _ => {}
-            },
-        }
-    }
 }
