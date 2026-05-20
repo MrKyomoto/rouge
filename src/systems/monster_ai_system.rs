10ef7a24b07634ea48def22f0b4ada1b21acf9ce
kyomoto-pixelbook <2028566723@qq.com>
2026-05-20 15:33:40 +0800

    src: test ai

--- src/systems/monster_ai_system.rs
+++ src/systems/monster_ai_system.rs
@@ -1,4 +1,4 @@
-use bracket_lib::prelude::console;
+use bracket_lib::prelude::{Point, console};
 use specs::prelude::*;
 
 use crate::components::{Monster, Player, Position, Viewshed};
@@ -13,9 +13,14 @@ impl<'a> System<'a> for MonsterAIStstem {
     );
 
     fn run(&mut self, data: Self::SystemData) {
-        let (monster, player, pos, viewshed) = data;
-        for (_monster, pos, viewshed) in (&monster, &pos, &viewshed).join() {
-            console::log("Monster considers its own existence");
+        let (monster, player, position, viewshed) = data;
+        for (_monster, mpos, viewshed) in (&monster, &position, &viewshed).join() {
+            for (_player, ppos) in (&player, &position).join() {
+                let pp = Point::new(ppos.x, ppos.y);
+                if viewshed.visible_tiles.contains(&pp) {
+                    console::log(format!("Monster found player and shouted"));
+                }
+            }
         }
     }
 }
