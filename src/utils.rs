ac7074d6a33043f2a0c22489f801617cf28fb855
kyomoto-pixelbook <2028566723@qq.com>
2026-05-20 21:20:31 +0800

    src: uitl fn index_2_xy

--- src/utils.rs
+++ src/utils.rs
@@ -7,3 +7,8 @@ pub const MAP_SIZE: usize = COL * ROW;
 pub fn flatten_index(x: i32, y: i32) -> usize {
     (y as usize * COL) + x as usize
 }
+pub fn index_2_xy(idx: usize, width: i32) -> (i32, i32) {
+    let x = idx as i32 % width;
+    let y = idx as i32 / width;
+    (x, y)
+}
